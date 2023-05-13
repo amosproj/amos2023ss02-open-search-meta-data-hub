1f52e603221418f9b1afaefcdc6ae1e33ab67234
JMiltner97 <jan.miltner@onlinehome.de>
2023-05-13 13:40:40 +0200

    Added Webserver Container
    Webserver to implement simple search for open search has been added
    
    Co-Authored-By: motrell <105235679+motrell@users.noreply.github.com>

---
+++ src/webserver/res/main.js
@@ -0,0 +1,13 @@
+function sendAlert(){
+    alert("Hello World!");
+}
+
+function search(){
+    $.ajax({
+        url: ".../mdh/_search",
+        
+        success: function( result ) {
+          $( "#searchField" ).html( "<strong>" + result + "</strong>" );
+        }
+      });
+}
