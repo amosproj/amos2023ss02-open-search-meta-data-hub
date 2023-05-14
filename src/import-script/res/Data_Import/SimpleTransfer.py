8c33ba9b394b513c1f713434383ea1ce058129dc
leondaniel22 <leondaniel22@web.de>
2023-05-14 21:06:49 +0200

    added a method to synchronize the opensearch import script and the opensearch node docker containers and minor bug fixes

--- src/import-script/res/Data_Import/SimpleTransfer.py
+++ src/import-script/res/Data_Import/SimpleTransfer.py
@@ -1,26 +1,39 @@
 import json
 import os
 import pathlib
+import time
 from opensearchpy import OpenSearch
+from opensearchpy.exceptions import ConnectionError
 
-# connect to OpenSearch
-host = 'localhost'
-port = 9200
+
+""" connect to OpenSearch """
+host = 'opensearch-node' # container name of the opensearch node docker container
+port = 9200 # port on which the opensearch node run s
 auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
-# ca_certs_path = '/full/path/to/root-ca.pem' # Provide a CA bundle if you use intermediate CAs with your root CA.
 
-# Create the client with SSL/TLS and hostname verification disabled.
+
+""" Create the client with SSL/TLS and hostname verification disabled. """
 client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_compress = True, # enables gzip compression for request bodies
-    use_ssl = False,
-    verify_certs = False,
-    ssl_assert_hostname = False,
-    ssl_show_warn = False
-)
+    hosts = [{'host': host, 'port': port}], # host and port to connect with
+    http_auth=auth, # credentials
+    use_ssl=False, # disable ssl
+    verify_certs=False, # disable verification of certificates
+    ssl_assert_hostname=False, # disable verification of hostname
+    ssl_show_warn=False, # disable ssl warnings
+    retry_on_timeout=True # enable the client trying to reconnect after a timeout
+    )
 
-# create a new index
-index_name = 'mdh-test-data'
+""" wait till node is ready before performing an import """
+print('\nConnection to OpenSearch Node ...')
+for _ in range(100):
+    try:
+        client.cluster.health(wait_for_status="yellow") # make sure the cluster is available
+    except ConnectionError:
+        time.sleep(2) # take a short break to give the opensearch node time to be fully set-up
+print('\nSuccessfully connected!')
+
+""" create a new index with not default settings """
+index_name = 'mdh-test-data' # the index name
 index_body = {
   'settings': {
     'index': {
@@ -28,26 +41,34 @@ index_body = {
     }
   }
 }
-#response = client.indices.create(index_name, body=index_body)
+response = client.indices.create(index_name, body=index_body) # create the index in the opensearch node
+print('\nCreating index:')
+print(response)
 
-# get the result json file from SimpleImport
-# mdh_file = SimpleImport.result
 
-# get a json test file that was manually extracted from the mdh
+""" get a json test file that was manually extracted from the mdh """
 basepath = os.path.dirname(__file__)
 new_path = os.path.join(basepath, 'Test_Data/test_file_1.json')
 print(new_path)
 with open(new_path, 'r') as f:
     mdh_file = json.load(f)
 
-# extract metadata from file
-new_json = {}
-for file in mdh_file['data']['mdhSearch']['files']:
-    for metadata in file['metadata']:
-        new_json[metadata['name']] = metadata['value']
+
+""" extract metadata from json file """
+new_json = {} # initialize a new json file
+# id = 1
+for file in mdh_file['data']['mdhSearch']['files']: # loop over all files
+    for metadata in file['metadata']: # loop over all metadata
+        new_json[metadata['name']] = metadata['value'] # save each metadata in the new json file
     print(new_json)
-    # response = client.index(
-    #     index=index_name,
-    #     body=new_json,
-    #     refresh=True
-    # )
+    # upload new json to the opensearch node
+    response = client.index(
+        index=index_name,
+        body=new_json,
+        # id = id,
+        refresh=True
+    )
+
+print('\nIndexing document:')
+print(response)
+
