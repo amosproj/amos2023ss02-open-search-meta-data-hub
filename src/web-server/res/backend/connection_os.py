6baca4e367ae5ddbfff0f37283ddc97a64dcb2d6
leondaniel22 <leondaniel22@web.de>
2023-05-18 17:55:57 +0200

    fixed bug
    Co-authored-by: Leon Daniel <leondaniel22@web.de>>

--- src/web-server/res/backend/connection_os.py
+++ src/web-server/res/backend/connection_os.py
@@ -2,33 +2,30 @@ from opensearchpy import OpenSearch
 import time
 
 
-
 def connect_to_os():
-
     """ connect to OpenSearch """
-    host = 'opensearch-node' # container name of the opensearch node docker container
-    port = 9200 # port on which the opensearch node runs
-    auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
-
+    host = 'opensearch-node'  # container name of the opensearch node docker container
+    port = 9200  # port on which the opensearch node runs
+    auth = ('admin', 'admin')  # For testing only. Don't store credentials in code.
 
     """ Create the client with SSL/TLS and hostname verification disabled. """
-    client = OpenSearch(
-        hosts = [{'host': host, 'port': port}], # host and port to connect with
-        http_auth=auth, # credentials
-        use_ssl=False, # disable ssl
-        verify_certs=False, # disable verification of certificates
-        ssl_assert_hostname=False, # disable verification of hostname
-        ssl_show_warn=False, # disable ssl warnings
-        retry_on_timeout=True # enable the client trying to reconnect after a timeout
+    # Create the client with SSL/TLS enabled, but hostname verification disabled.
+    try:
+        client = OpenSearch(
+            hosts=[{'host': host, 'port': port}],
+            http_compress=True,  # enables gzip compression for request bodies
+            http_auth=auth,
+            # client_cert = client_cert_path,
+            # client_key = client_key_path,
+            use_ssl=True,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False,
+            # ca_certs = ca_certs_path
         )
-
-    """ wait till node is ready before performing an import """
-    print('\nConnection to OpenSearch Node ...')
-    for _ in range(100):
-        try:
-            client.cluster.health(wait_for_status="yellow") # make sure the cluster is available
-        except ConnectionError:
-            time.sleep(2) # take a short break to give the opensearch node time to be fully set-up
-    print('\nSuccessfully connected!')
+    except BaseException:
+        # connection error; send error message
+        print("ERROR unable to connect opensearch instance")
+        return False
 
     return client
