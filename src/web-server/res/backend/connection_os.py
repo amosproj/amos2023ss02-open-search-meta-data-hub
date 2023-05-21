9a7dc0fdc1efe3a13a547e45cbc974da55461328
Max Ole Elliger <ole.elliger@fau.de>
2023-05-21 16:25:32 +0200

    Implemented API for connection
    between frontend and backend on the new Web architecture
    
    Co-authored-by: JMiltner97 <jan.miltner@fau.de>
    
    Signed-off-by: Max Ole Elliger <ole.elliger@fau.de>

--- src/web-server/res/backend/connection_os.py
+++ src/web-server/res/backend/connection_os.py
@@ -16,7 +16,7 @@ def connect_to_os():
             http_auth=auth,
             # client_cert = client_cert_path,
             # client_key = client_key_path,
-            use_ssl=True,
+            use_ssl=False,
             verify_certs=False,
             ssl_assert_hostname=False,
             ssl_show_warn=False,
