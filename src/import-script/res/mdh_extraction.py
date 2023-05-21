2d657e11cfc5217af0518ea139c324a999145ead
leondaniel22 <leondaniel22@web.de>
2023-05-21 20:21:32 +0200

    implemented efficient data-transfer
    Co-authored-by: Amir Houssaen <miro.apple94@gmail.com>

--- src/import-script/res/mdh_extraction.py
+++ src/import-script/res/mdh_extraction.py
@@ -12,16 +12,16 @@ os.environ['MDH_HOME'] = '/WORK_REPO/mdh_home'
 
 
 """ create the path where to search for data"""
-request_path_file=os.path.join(os.getcwd(),"request.gql")
+request_path_file = os.path.join(os.getcwd(), "request.gql")
 
-""" Intitialization of the MDH-Core"""
+""" Initialization of the MDH-Core"""
 mdh.init()
 # if(mdh.core.main.get().count>0):
 
-""" Connecion to MDH-Core """
+""" Connection to MDH-Core """
 mdh.core.main.add(
-url=os.getenv("URL_CORE_1"),
-password_user=os.getenv("PW_USER_CORE_1")
+    url=os.getenv("URL_CORE_1"),
+    password_user=os.getenv("PW_USER_CORE_1")
 )
 
 """ get a json file with sample-data from the metadata-hub """
