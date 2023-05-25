6f39aae068e3d682ae2c3d53e4211127bdd8cf14
leondaniel22 <leondaniel22@web.de>
2023-05-25 21:14:17 +0200

    Created an advanced search skelton function
    Co-authored-by: Amir-Hussein-OTH <miro.apple94@gmail.com>

--- src/web-server/res/backend/search_os.py
+++ src/web-server/res/backend/search_os.py
@@ -15,3 +15,27 @@ def simple_search(client: OpenSearch, search_text):
         index='amoscore'
     )
     return response
+
+def advanced_search(client: OpenSearch, search_info: dict):
+    # This is just a sample idea of how to pass search information from frontend to backend
+    search_info = {
+        'FileName': {
+            'search_content': 'medicine',
+            'operator': '=',
+            'boost': 2
+        },
+        'FileSize': {
+            'search_content': 500,
+            'operator': '>',
+            'boost': 2
+        },
+        'FileNodeChangeDate': '2013-05-05T08:00:00',
+        'operator': '>',
+        'boost': 3
+    }
+    query = {}
+    return
+
+
+
+
