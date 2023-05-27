11c4537b90b0fb82a68a28ede1bccb36407789fd
Omar Ninach <omar.ninach@fau.de>
2023-05-27 10:56:39 +0200

    apply pytlint to import-script files
    Co-authored-by: Max Ole Elliger <ole.elliger@fau.de>
    Co-authored-by: Omar Ninach <omar.ninach@fau.de>

--- lint.py
+++ lint.py
@@ -1,14 +1,41 @@
-import sys
+# import sys
 
-from pylint.lint import Run
+# from pylint.lint import Run
+
+# THRESHOLD = 5
+# score = Run(["test.py"], exit=False).linter.stats.global_note
+
+
+
+# print(score)
+# if score < THRESHOLD:
+#     print("Linter failed: Score < threshold value")
+#     sys.exit(1)
+# sys.exit(0)
+import os
+import sys
+from pylint import epylint as lint
 
 THRESHOLD = 5
-score = Run(["test.py"], exit=False).linter.stats.global_note
+folder_path = "src/import-script/res"
+
+# Get a list of all Python files in the folder
+file_list = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if filename.endswith(".py")]
+
+# Run pylint on each file
+lint_output = []
+for file_path in file_list:
+    (pylint_stdout, _) = lint.py_run(file_path, return_std=True)
+    lint_output.append(pylint_stdout.getvalue())
 
 
+print(lint_output)
+# Calculate the average score
+scores = [float(output.split(":")[1].strip()) for output in lint_output]
+average_score = sum(scores) / len(scores)
 
-print(score)
-if score < THRESHOLD:
-    print("Linter failed: Score < threshold value")
+print("Average score:", average_score)
+if average_score < THRESHOLD:
+    print("Linter failed: Average score < threshold value")
     sys.exit(1)
 sys.exit(0)
