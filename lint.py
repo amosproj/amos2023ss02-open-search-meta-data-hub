96b4ec82dbf4b081ab78c571afb23fc448524374
Omar Ninach <omar.ninach@fau.de>
2023-05-29 11:54:20 +0200

    commit to test
    Co-authored-by: Max Ole Elliger <ole.elliger@fau.de>

--- lint.py
+++ lint.py
@@ -1,23 +1,25 @@
-import os
 import sys
-from pylint import epylint as lint
+import subprocess
+from pylint import lint
 
+# Perfect code: Some organizations strive for perfection and aim for a pylint score of 10. 
+# Good code: Many organizations consider a pylint score between 8 and 9 as acceptable. 
+# Acceptable code: For less critical projects or projects with specific constraints, a pylint score between 6 and 7 might be acceptable. 
+# Minimum threshold: In some cases, a project may have a minimum threshold to ensure a basic level of code quality. This threshold could be set anywhere between 4 and 6.
+ 
 THRESHOLD = 5
-folder_path = "test"
-# folder_path = "src/import-script/res"
+folder_path = "."
 
-# Get a list of all Python files in the folder
-file_list = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if filename.endswith(".py")]
+file_list = subprocess.check_output(["find", folder_path, "-type", "f", "-name", "*.py"]).decode().splitlines()
 
-# Run pylint on each file
-lint_output = []
+scores = []
 for file_path in file_list:
-    (pylint_stdout, _) = lint.py_run(file_path, return_std=True)
-    lint_output.append(pylint_stdout.getvalue())
+    run = lint.Run(["--rcfile=.pylintrc", file_path], do_exit=False)
+    score = run.linter.stats.global_note
+    scores.append(score)
 
-print(lint_output)
-# Calculate the average score
-scores = [float(output.split(":")[1].strip()) for output in lint_output]
+print("Scores:", scores)
+print("Length:", len(scores))
 average_score = sum(scores) / len(scores)
 
 print("Average score:", average_score)
@@ -25,3 +27,35 @@ if average_score < THRESHOLD:
     print("Linter failed: Average score < threshold value")
     sys.exit(1)
 sys.exit(0)
+#-------------------------------
+
+# import os
+# import sys
+# from pylint import lint
+
+# THRESHOLD = 5
+# folder_path = "src/import-script/res"
+
+# # Get a list of all Python files in the folder
+# file_list = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if filename.endswith(".py")]
+
+# # Run pylint on each file
+# lint_output = []
+# for file_path in file_list:
+#     run = lint.Run(["--rcfile=.pylintrc", file_path], do_exit=False)
+#     score = run.linter.stats.global_note
+#     lint_output.append(score)
+
+# print(lint_output)
+# # Calculate the average score
+# scores = [float(output) for output in lint_output]
+
+# print("Scores: ", scores)
+# print("Length: ", len(scores))
+# average_score = sum(scores) / len(scores)
+
+# print("Average score: ", average_score)
+# if average_score < THRESHOLD:
+#     print("Linter failed: Average score < threshold value")
+#     sys.exit(1)
+# sys.exit(0)
