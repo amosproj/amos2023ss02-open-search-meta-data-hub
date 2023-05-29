91485016910205ae0134ae6f4eff0f9d8af8d2ab
Omar Ninach <omar.ninach@fau.de>
2023-05-29 12:23:06 +0200

    commit to test
    Co-authored-by: Max Ole Elliger <ole.elliger@fau.de>

--- src/pylint/lint.py
+++ src/pylint/lint.py
@@ -2,19 +2,21 @@ import sys
 import subprocess
 from pylint import lint
 
-# Perfect code: Some organizations strive for perfection and aim for a pylint score of 10. 
-# Good code: Many organizations consider a pylint score between 8 and 9 as acceptable. 
-# Acceptable code: For less critical projects or projects with specific constraints, a pylint score between 6 and 7 might be acceptable. 
-# Minimum threshold: In some cases, a project may have a minimum threshold to ensure a basic level of code quality. This threshold could be set anywhere between 4 and 6.
- 
-THRESHOLD = 3
-folder_path = "."
+# "Perfect code": Some organizations strive for perfection and aim for a pylint score of 10.
+# "Good code": Many organizations consider a pylint score between 8 and 9 as acceptable.
+# "Acceptable" code: For less critical projects or projects with specific constraints, 
+# a pylint score between 6 and 7 might be acceptable.
+# "Minimum threshold": In some cases, a project may have a minimum threshold to ensure a basic level of code quality.
+# This threshold could be set anywhere between 4 and 6.
 
-file_list = subprocess.check_output(["find", folder_path, "-type", "f", "-name", "*.py"]).decode().splitlines()
+THRESHOLD = 5
+FOLDER_PATH = "."
+
+file_list = subprocess.check_output(["find", FOLDER_PATH, "-type", "f", "-name", "*.py"]).decode().splitlines()
 
 scores = []
 for file_path in file_list:
-    run = lint.Run(["--rcfile=.pylintrc", file_path], do_exit=False)
+    run = lint.Run(["--rcfile=src/pylint/.pylintrc", file_path], do_exit=False)
     score = run.linter.stats.global_note
     scores.append(score)
 
