54e59d92a023832861dc49b11576f08e7cc54a79
JMiltner97 <jan.miltner@onlinehome.de>
2023-06-04 13:32:16 +0200

    (Rebase Commit) Added first draft of the Flask Forms functionality to communicate between the frontend and backend
    Added first draft of the Flask Forms functionality to communicate between the frontend and backend
    
    Signed-off-by: JMiltner97 <jan.miltner@onlinehome.de>
    Co-Authored-By: motrell <105235679+motrell@users.noreply.github.com>

--- src/application/res/app.py
+++ src/application/res/app.py
@@ -2,6 +2,9 @@ import json
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, FieldList, FormField, Form
+from wtforms.validators import DataRequired
 from backend.opensearch_api import OpenSearchManager
 from backend import files_type
 import urllib
@@ -9,6 +12,15 @@ import urllib
 app = Flask(__name__)
 index_name = 'amoscore'
 os_manager: OpenSearchManager = OpenSearchManager()
+
+
+class ItemForm(Form):
+    tag = StringField('tag', validators=[DataRequired()])
+    searchOperator = SelectField('searchOperator', [('tag_exists', 'tag exists'),('tag_not_exist', 'tag not exists'),('', '────────────────'), ('field_is_empty', 'field is empty'),('field_is_not_empty', 'field is not empty'),('', '────────────────'),('contains', 'contains'),('not_contains', 'not contains'),('', '────────────────'),('is_equal', 'is equal'),('is_not_equal', 'is not equal'),('', '────────────────'),('is_greater', 'is greater'),('is_smaller', 'is smaller')], validators=[DataRequired()])
+    searchValue = StringField('searchValue', validators=[DataRequired()])
+
+class advancedSearchForm(FlaskForm):
+    items = FieldList(FormField(ItemForm), min_entries=1)
 
 """Rendering start page of the website"""
 
@@ -29,14 +41,20 @@ def search():
 
 @app.route('/search/simple')
 def search_simple():
-    return os_manager.simple_search(index_name, request.args.get('searchString'))
-
+    return os_manager.simple_search("amoscore", request.args.get('searchString')) #Hardcoded indexname
 
 @app.route('/search/advanced')
 def search_advanced():
-    search_info = json.loads(urllib.parse.unquote(request.args.get('searchString')))
-    return os_manager.advanced_search(index_name, search_info)
+    search_info = json.loads(urllib.parse.unquote(request.args.get('searchString'))) #ToDo use Flask Forms
+    return os_manager.advanced_search("amoscore", search_info) #Hardcoded indexname
 
+@app.route('/search/advanced_v2')
+def advanced_search_v2():
+    form = advancedSearchForm()
+    if form.validate_on_submit():
+        # ... 
+        pass
+    return render_template('index1.html')
 
 @app.route('/files_type_chart')
 def files_type_chart():
