69f0897691f6d6558e2e0b26ccb1e917f7b1f6e2
Max Ole Elliger <ole.elliger@fau.de>
2023-06-09 13:08:14 +0200

    Some Refactoring on get_all_iframes.py
    Removed second for-loop (now it is more like a pipeline)
    reordered some code
    
    Signed-off-by: Max Ole Elliger <ole.elliger@fau.de>

--- src/application/res/backend/get_all_iframes.py
+++ src/application/res/backend/get_all_iframes.py
@@ -11,21 +11,18 @@ def get_iframes():
 
     response = requests.get(url, headers=headers)
     response_data = response.json()
+    visualizations = response_data.get('saved_objects', [])
 
-    # Extract visualization IDs
-    visualization_ids = []
-    for obj in response_data.get('saved_objects', []):
-        if obj.get('id') and obj.get('type') == 'visualization':
-            visualization_ids.append(obj['id'])
-
-    # Generate the iframe code
     iframe_codes = []
-    for visualization_id in visualization_ids:
-        encoded_id = urllib.parse.quote(visualization_id)
-        iframe_code = f'<iframe src="http://localhost:5601/app/visualize#/edit/{encoded_id}?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="600" width="800"></iframe>'
-        iframe_codes.append(iframe_code)
+    for obj in visualizations:
+        if obj.get('id') and obj.get('type') == 'visualization':
+
+            # Extract visualization ID
+            visualization_id = obj['id']
+            encoded_id = urllib.parse.quote(visualization_id)
+
+            # Generate the iframe code
+            iframe_code = f'<iframe src="http://localhost:5601/app/visualize#/edit/{encoded_id}?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="600" width="800"></iframe>'
+            iframe_codes.append(iframe_code)
 
     return iframe_codes
-    # Print the iframe codes
-    # for iframe_code in iframe_codes:
-    #     print(iframe_code)
