b0aeebc70c51f71f84eb57417a1c8e8b2fa817fe
Amir-Hussein-OTH <miro.apple94@gmail.com>
2023-06-10 13:55:08 +0200

    fix the issue is that the last_import_timestamp variable is None.
    The error message indicates that the > operator is not supported between NoneType and datetime.datetime instances. This error occurs in the modify_data function when comparing the file_timestamp with the last_import_timestamp:
    
    Signed-off-by:  Amir-Hussein-OTH <miro.apple94@gmail.com>

--- src/application/res/import-script/import_pipeline.py
+++ src/application/res/import-script/import_pipeline.py
@@ -23,10 +23,6 @@ def update_last_import_timestamp(timestamp):
         with open('last_import_timestamp.txt', 'w') as file:
             file.write(str(timestamp))
     except IOError:
-        """
-                     with comments to indicate areas that can be discussed with other developers:
-             """
-
         # Handle the case when an error occurs while writing the timestamp file
         # You can choose to raise an exception or handle the error gracefully
         # Example: raise IOError("Error occurred while writing the last import timestamp file")
@@ -34,33 +30,13 @@ def update_last_import_timestamp(timestamp):
 
 
 def load_last_import_timestamp():
-    """
-    Load the last import timestamp from a file.
-
-    :return: The last import timestamp as a datetime object, or None if it couldn't be loaded.
-    """
     try:
         with open('last_import_timestamp.txt', 'r') as file:
-            timestamp_str = file.read().strip()
-            timestamp = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
-            return timestamp
+            timestamp = file.read()
+            return datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
     except FileNotFoundError:
-        """
-                with comments to indicate areas that can be discussed with other developers:
-        """
-
-        # Handle the case when the file doesn't exist
-        # Example: return datetime(2023, 1, 1, 0, 0, 0)  # Return a default timestamp
-        # Example: raise FileNotFoundError("Last import timestamp file not found")  # Raise an exception
-        return None
-    except ValueError:
-        """
-                with comments to indicate areas that can be discussed with other developers:
-             """
-        # Handle the case when the timestamp format in the file is invalid
-        # Example: return datetime(2023, 1, 1, 0, 0, 0)  # Return a default timestamp
-        # Example: raise ValueError("Invalid timestamp format in the file")  # Raise an exception
-        return None
+        # Handle the case when the file is not found
+        return datetime.utcnow()  # Return the current timestamp as the default value
 
 
 def modify_datatypes(mdh_datatypes):
@@ -134,38 +110,6 @@ def transform_data(mdh_data, data_types):
     return transformed_data
 
 
-def modify_data(mdh_data: list[dict], data_types: dict) -> list[dict]:
-    """
-    Reformatting the mdh_data dictionary for storage in OpenSearch.
-
-    :param mdh_data: A list of dictionaries that contains all metadata-tags for every file of a MetaDataHub request.
-    :param data_types: A dictionary containing the modified OpenSearch datatypes.
-    :return: A list of dictionaries containing the modified metadata tags and their corresponding values.
-    """
-
-    modified_data = []  # init the resulting list
-    for index, file_data in enumerate(mdh_data, start=1):  # Loop over all files of mdh_data
-        metadata = file_data.get("metadata", [])  # get the metadata for each file
-        file_info = {}  # init the dictionary in which the metadata will be stored
-        for meta in metadata:  # loop over every metadata-tag
-            name = str(meta.get("name")).replace(".",
-                                                 "_")  # get the name and replace '. with '_' to avoid parsing errors
-            value = meta.get("value")  # get the corresponding value for the metadata-tag
-            # set correct datatypes
-            if data_types[name] == 'date':
-                date = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")  # get the correct datetime format
-                value = str(date.utcnow().strftime(
-                    "%Y-%m-%d" "T" "%H:%M:%S"))  # make the datetime a string so it can be stored in OpenSearch
-            elif data_types[name] == 'float':
-                value = float(value)
-            if name and value:
-                file_info[name] = value
-
-        modified_data.append(file_info)
-
-    return modified_data
-
-
 def generate_mdh_search_query(filter_functions=None, limit=2000):
     """
     Generate a dynamic MDH search query.
@@ -216,6 +160,42 @@ def generate_mdh_search_query(filter_functions=None, limit=2000):
 # example of usage
 # query_string = generate_mdh_search_query(filter_functions=["your", "filter", "functions"], limit=500)
 # print(query_string)
+def modify_data(mdh_data: list[dict], data_types: dict, last_import_timestamp: datetime) -> list[dict]:
+    """
+    Reformatting the mdh_data dictionary for storage in OpenSearch.
+
+    :param mdh_data: A list of dictionaries that contains all metadata-tags for every file of a MetaDataHub request.
+    :param data_types: A dictionary containing the modified OpenSearch datatypes.
+    :param last_import_timestamp: The timestamp of the last import to OpenSearch.
+    :return: A list of dictionaries containing the modified metadata tags and their corresponding values.
+    """
+
+    modified_data = []  # init the resulting list
+    for index, file_data in enumerate(mdh_data, start=1):  # Loop over all files of mdh_data
+        metadata = file_data.get("metadata", [])  # get the metadata for each file
+        file_info = {}  # init the dictionary in which the metadata will be stored
+        file_timestamp = file_data.get("timestamp")
+
+        # Check if the file has been modified or added since the last import
+        if file_timestamp > last_import_timestamp:
+            for meta in metadata:  # loop over every metadata-tag
+                name = str(meta.get("name")).replace(".",
+                                                     "_")  # get the name and replace '. with '_' to avoid parsing errors
+                value = meta.get("value")  # get the corresponding value for the metadata-tag
+                # set correct datatypes
+                if data_types[name] == 'date':
+                    date = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")  # get the correct datetime format
+                    value = str(date.utcnow().strftime(
+                        "%Y-%m-%d" "T" "%H:%M:%S"))  # make the datetime a string so it can be stored in OpenSearch
+                elif data_types[name] == 'float':
+                    value = float(value)
+                if name and value:
+                    file_info[name] = value
+
+            modified_data.append(file_info)
+
+    return modified_data
+
 
 if __name__ == "__main__":
     print("Start importing...")
