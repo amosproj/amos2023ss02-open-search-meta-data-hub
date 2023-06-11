a41de25f74e13550d657cdc1aa2db9409f1fe861
Omar Ninach <omar.ninach@fau.de>
2023-06-11 18:17:30 +0200

    fix timezone

--- src/application/res/backend/get_all_iframes.py
+++ src/application/res/backend/get_all_iframes.py
@@ -2,6 +2,7 @@ import requests
 import urllib.parse
 import json
 from datetime import datetime
+from dateutil import tz
 
 
 
@@ -20,17 +21,17 @@ def get_iframes():
     iframe_data = []
     for obj in visualizations:
         if obj.get('id') and obj.get('type') == 'visualization':
+
             # Extract visualization information
             visualization_id = obj['id']
             encoded_id = urllib.parse.quote(visualization_id)
             title = obj['attributes']['title'].capitalize()
             vis_state = json.loads(obj['attributes']['visState'])
-            vis_type = vis_state['type'].capitalize()            
-            updated_at = obj['updated_at']
-            updated_at_datetime = datetime.fromisoformat(updated_at[:-1])
-            updated_at_human_readable = updated_at_datetime.strftime('%b %d, %Y - %H:%M:%S')
+            vis_type = vis_state['type'].capitalize()     
 
-
+            #get and convert last update time            
+            updated_at_human_readable=convert_time(obj['updated_at'])
+            
             # Generate the iframe code
             iframe_code = f'<iframe src="http://localhost:5601/app/visualize#/edit/{encoded_id}?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="600" width="800"></iframe>'
 
@@ -45,3 +46,9 @@ def get_iframes():
     return iframe_data
 
 
+def convert_time(updated_at_ts):
+  utc_dt = datetime.fromisoformat(updated_at_ts[:-1]).replace(tzinfo=tz.tzutc())
+  german_tz = tz.gettz('Europe/Berlin')
+  german_dt = utc_dt.astimezone(german_tz)
+  updated_at_human_readable = german_dt.strftime('%b %d, %Y - %H:%M:%S')
+  return updated_at_human_readable
