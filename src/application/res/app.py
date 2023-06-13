f0d7a7dff32d8e666478c2f0ec368cc736c1bb97
leondaniel22 <leondaniel22@web.de>
2023-06-13 22:34:56 +0200

    Changed localhost setting

--- src/application/res/app.py
+++ src/application/res/app.py
@@ -18,7 +18,7 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 bootstrap = Bootstrap5(app)
 csrf = CSRFProtect(app)
 
-os_manager: OpenSearchManager = OpenSearchManager(localhost=True)
+os_manager: OpenSearchManager = OpenSearchManager(localhost=False)
 
     
 class SimpleSearchForm(FlaskForm):
