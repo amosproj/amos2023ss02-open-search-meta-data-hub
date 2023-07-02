3f0e308f4062c3b41a7cd02f719063f2e69da5b8
Amir-Hussein-OTH <miro.apple94@gmail.com>
2023-07-02 10:01:59 +0200

    Add comments to app initialization code
    This commit adds descriptive comments to the code that initializes the Flask app.
    These comments enhance code readability and provide a better understanding of the app initialization process.
    
    Co-authored-by: leondaniel22 <leondaniel22@web.de>
    Signed-off-by: Amir-Hussein-OTH <miro.apple94@gmail.com>

--- src/application/res/app.py
+++ src/application/res/app.py
@@ -14,28 +14,38 @@ import pandas as pd
 import urllib
 import configparser
 
+# Configuration Setup
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# Flask Application Setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 
+# Flask Extensions
 bootstrap = Bootstrap5(app)
 csrf = CSRFProtect(app)
 
+# OSDashboardManager Initialization
 os_dashboard_manager: OSDashboardManager = OSDashboardManager(localhost=config.getboolean('General', 'localhost'))
+
+# OpenSearchManager Initialization
 os_manager: OpenSearchManager = OpenSearchManager(localhost=config.getboolean('General', 'localhost'),
                                                   search_size=config.getint('General', 'search_size'))
 
 
+# SimpleSearchForm Definition
 class SimpleSearchForm(FlaskForm):
     searchValue = StringField('Search Value', validators=[DataRequired()])
     submit = SubmitField('Search')
 
 
 class AdvancedEntryForm(FlaskForm):
+    # Get all available fields for the specified index
     all_fields = os_manager.get_all_fields(index_name=config.get('General', 'default_index_name'))
+    # Dropdown menu for selecting a metadata tag
     metadata_tag = SelectField('Metadata tag', choices=all_fields)
+    # Dropdown menu for selecting a condition for the metadata tag
     condition = SelectField('Condition', choices=[
         ('tag_exists', 'tag exists'),
         ('tag_not_exist', 'tag not exists'),
@@ -47,14 +57,26 @@ class AdvancedEntryForm(FlaskForm):
         ('is_not_equal', 'is not equal'),
         ('is_greater', 'is greater'),
         ('is_smaller', 'is smaller')])
+    # Text field for entering a value associated with the metadata tag
     value = StringField('Value')
+    # Integer field for assigning a weight to the entry
     weight = IntegerField('Weight',
                           [validators.NumberRange(min=1, max=100, message="Weight must be between 1 and 100")],
                           default=1)
 
 
+# Define a form class for advanced search functionality
 class AdvancedSearchForm(FlaskForm):
+    """
+    Form class for advanced search functionality.
+
+    The form allows users to define multiple search criteria by adding and configuring entries.
+    Each entry is represented by an instance of the AdvancedEntryForm form class.
+    The form provides fields for specifying metadata tag, condition, value, and weight for the search criteria.
+    """
+    # Field for defining multiple search criteria entries
     entry = FieldList(FormField(AdvancedEntryForm), min_entries=1)
+    # Submit button for submitting the form
     submit = SubmitField('Submit')
 
 
@@ -94,43 +116,69 @@ def index():
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
+    # Set the last_form session variable to 'simple'
     session['last_form'] = 'simple'
+
+    # Initialize SimpleSearchForm and AdvancedSearchForm
     simpleSearchForm = SimpleSearchForm()
     simpleSearchResult = ""
     advancedSearchForm = AdvancedSearchForm()
     advancedSearchResult = ""
+
+    # Extract field names and data types from 'amoscore' and convert to JSON string
     field_names_data_types = os_manager.extract_metadata_dict('amoscore')
     json_dict = json.dumps(field_names_data_types)
+
+    # Handle simple search form submission
     if simpleSearchForm.validate_on_submit():
+        # Get the search value from the form
         searchValue = simpleSearchForm.searchValue.data
+
+        # Perform simple search using the default index name
         resultTmp = os_manager.simple_search(config.get('General', 'default_index_name'), searchValue)
+
+        # Set the last_form session variable to 'simple'
         session['last_form'] = 'simple'
+
+        # Render the search results or display a message if no results found
         if len(resultTmp) > 0:
             simpleSearchResult = renderResult(resultTmp)
         else:
             simpleSearchResult = "No results found."
 
+    # Handle advanced search form submission
     if advancedSearchForm.validate_on_submit():
+        # Initialize a dictionary to store search information
         search_info = {}
+
+        # Iterate over each entry in the advanced search form
         for entry in advancedSearchForm.entry.data:
             parameter_name = entry['metadata_tag']
             search_content = entry['value']
             operator = entry['condition']
             weight = entry['weight']
+
+            # Store the search information in the dictionary
             search_info[parameter_name] = {
                 'search_content': search_content,
                 'operator': operator,
                 'weight': weight
             }
-            print(search_info)
+
+        # Perform advanced search using the default index name and the search information
         resultTmp = os_manager.advanced_search(index_name=config.get('General', 'default_index_name'),
                                                search_info=search_info)
+
+        # Set the last_form session variable to 'advanced'
         session['last_form'] = 'advanced'
+
+        # Render the search results or display a message if no results found
         if len(resultTmp) > 0:
             advancedSearchResult = renderResult(resultTmp)
         else:
             advancedSearchResult = "No results found."
 
+    # Render the search.html template with form data, search results, and other variables
     return render_template('search.html', simpleSearchForm=simpleSearchForm, simpleSearchResult=simpleSearchResult,
                            advancedSearchForm=advancedSearchForm, advancedSearchResult=advancedSearchResult,
                            last_form=session.get('last_form'), json_dict=json_dict)
@@ -147,15 +195,25 @@ def search():
 
 @app.route('/search/advanced_v2')
 def advanced_search_v2():
+    # Render the index1.html template for advanced search version 2
     return render_template('index1.html')
 
 
 @app.route('/visualizations')
 def visualizations():
+    # Get the iframe data from the OS dashboard manager
     iframe_data = os_dashboard_manager.get_iframes()
 
+    # Render the visualizations.html template, passing the iframe data
     return render_template('visualizations.html', iframe_data=iframe_data)
 
 
 if __name__ == '__main__':
+    # Entry point of the script
+
+    # Run the Flask app
+    # The following command starts the Flask application and launches a development server.
+    # The app will listen for incoming requests on host '0.0.0.0' and port 8000.
+    # The server will continue running until it is manually stopped.
     app.run(host='0.0.0.0', port=8000)
+
